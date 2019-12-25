fd2153677606101903f5b919078f807b1400fd16
youwho42 <julianthompson@Julians-MacBook-Pro.local>
2019-12-25 12:59:24 +0100

    Platformer has art and paralax scrolling

---
+++ Assets/Scripts/Platformer/CursyBlinking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursyBlinking : MonoBehaviour
+{
+
+
+    SpriteRenderer sprite;
+    Color actualColor;
+    public Color blinkColor;
+    public float blinkRate =.5f;
+    
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        actualColor = sprite.color;
+        
+        InvokeRepeating("Blink", 0, blinkRate);
+    }
+
+    void Blink()
+    {
+        if(sprite.color == actualColor)
+        {
+            sprite.color = blinkColor;
+        }
+        else
+        {
+            sprite.color = actualColor;
+        }
+    }
+}
---
+++ Assets/Scripts/Platformer/ParalaxScrolling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxScrolling : MonoBehaviour
+{
+
+    float length, startpos;
+    public GameObject cam;
+    public float paralaxAmount;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float temp = cam.transform.position.x * (1 - paralaxAmount);
+        float dist = cam.transform.position.x * paralaxAmount;
+
+        //transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = Vector2.Lerp(transform.position, new Vector3(startpos + dist, transform.position.y, transform.position.z), 1);
+
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
