f902315db4fbc06745e07abd4c45056068a73bd0
youwho42 <julianthompson@Julians-MacBook-Pro.local>
2020-03-10 11:20:56 +0100

    flickering lights and trick or treat

---
+++ Assets/Scripts/Platformer/EnemyTrickOrTreat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrickOrTreat : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            AudioManager.instance.PlaySound("TrickOrTreat");
+        }
+    }
+}
---
+++ Assets/Scripts/Platformer/FlickeringLights.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class FlickeringLights : MonoBehaviour
+{
+
+    public Light2D lightToFlicker;
+    [Range(0.0f,1.0f)]
+    public float chanceToFlicker;
+    [Range(0.0f, 1.0f)]
+    public float flickerRate;
+    public Vector2 minMaxTimeBetweenFlickers;
+    float timeToNextFlicker;
+    float currentFlickerTime;
+    bool willFlicker;
+    bool isFlickering;
+
+    private void Start()
+    {
+        float rand = Random.Range(0.0f, 1.0f);
+        if(rand <= chanceToFlicker)
+        {
+            willFlicker = true;
+            
+        }
+    }
+
+    private void Update()
+    {
+        if (willFlicker)
+        {
+            currentFlickerTime += Time.deltaTime;
+            if(currentFlickerTime >= timeToNextFlicker && !isFlickering)
+            {
+                isFlickering = true;
+                StartCoroutine("FlickerLightCo");
+                currentFlickerTime = 0;
+            }
+        }
+    }
+
+    IEnumerator FlickerLightCo()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            float rand = Random.Range(0.0f, 1.0f);
+            lightToFlicker.intensity = rand;
+            yield return new WaitForSeconds(flickerRate);
+        }
+        timeToNextFlicker = Random.Range(minMaxTimeBetweenFlickers.x, minMaxTimeBetweenFlickers.y);
+        isFlickering = false;
+        lightToFlicker.intensity = 1.0f;
+        yield return null;
+    }
+}
